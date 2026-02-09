83fbe37ffbcafa653a436167264b2e0057e84398
NanYuanZhiGe <NanYuanZhiGe@outlook.com>
2026-02-09 19:47:22 +0800

    【功能添加】【模块上线】dock模块websocket测试-第1次

--- backend/SwiftSail/dock/src/main/java/com/nyzg/dock/conf/WebSocketConfig.java
+++ backend/SwiftSail/dock/src/main/java/com/nyzg/dock/conf/WebSocketConfig.java
@@ -1,13 +1,22 @@
 package com.nyzg.dock.conf;
 
+import com.nyzg.dock.controller.LongSyncController;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
 public class WebSocketConfig {
     @Bean
-    public ServerEndpointExporter serverEndpointExporter() {
-        return new ServerEndpointExporter();
+    public WebSocketConfigurer webSocketConfigurer() {
+        return new WebSocketConfigurer() {
+            @Override
+            public void registerWebSocketHandlers(@NotNull WebSocketHandlerRegistry registry) {
+                registry.addHandler(new LongSyncController(), "/ws/dock/websocket/report/sync");
+            }
+        };
     }
 }
--- backend/SwiftSail/dock/src/main/java/com/nyzg/dock/controller/LongSyncController.java
+++ backend/SwiftSail/dock/src/main/java/com/nyzg/dock/controller/LongSyncController.java
@@ -1,35 +1,16 @@
 package com.nyzg.dock.controller;
 
-import jakarta.websocket.*;
-import jakarta.websocket.server.ServerEndpoint;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
-import org.springframework.web.socket.server.standard.SpringConfigurator;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-@Component
-@ServerEndpoint(
-        value = "/ws/dock/websocket/report/sync",
-        configurator = SpringConfigurator.class
-)
 @Slf4j
-public class LongSyncController {
-    @OnOpen
-    public void webSocketTest(Session session, EndpointConfig config){
-
-    }
-
-    @OnMessage
-    public void onMessage(String message, Session session) {
-        log.info("message get: "+message);
-    }
-
-    @OnClose
-    public void onClose(Session session, CloseReason closeReason) {
-
-    }
-
-    @OnError
-    public void onError(Session session, Throwable throwable) {
-
+public class LongSyncController extends TextWebSocketHandler {
+    @Override
+    protected void handleTextMessage(@NotNull WebSocketSession session, @NotNull TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+        log.info("message get: "+message.getPayload());
     }
 }
