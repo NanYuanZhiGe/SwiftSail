f71153e6f5c2388af85aa1593254347545de9eea
NanYuanZhiGe <NanYuanZhiGe@outlook.com>
2026-02-22 00:43:44 +0800

    【模块部署】user模块，修正了新版本nacos拉取配置的方式

--- backend/SwiftSail/user/src/main/java/com/nyzg/user/UserApplication.java
+++ backend/SwiftSail/user/src/main/java/com/nyzg/user/UserApplication.java
@@ -3,9 +3,11 @@ package com.nyzg.user;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.nyzg")
+@EnableDiscoveryClient//新版nacos需要
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
