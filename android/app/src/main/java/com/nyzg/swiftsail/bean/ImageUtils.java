9f712f9e46cebea017f85b4cb4fd4045df2b46fc
NanYuanZhiGe <NanYuanZhiGe@outlook.com>
2026-03-01 23:19:32 +0800

    编写了geo的提交用户信息模块

--- android/app/src/main/java/com/nyzg/swiftsail/bean/ImageUtils.java
+++
@@ -1,9 +0,0 @@
-package com.nyzg.swiftsail.bean;
-
-import java.net.URI;
-
-public class ImageUtils {
-    public static String getHeadIconURI(long userId) {
-        return ServerURL.BASE_IMAGE_URL + userId + ServerURL.HEAD_ICON_SUFFIX;
-    }
-}
